84d7eeff0157b85d2f219cb0aa63951a9b8cb21d
Albert <albert@Alberts-Mac.local>
2019-10-25 20:38:57 -0400

    20191025: generate pages

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # ionic-loginsignup, 
 
+https://www.9lessons.info/2019/08/ionic-5-angular-8-welcome-page.html
+
 ## Setup project
 
  - `npm install -g ionic`
@@ -10,6 +12,32 @@ Mac OS, if with permission problem of folder "/usr/local/lib", then use `sudo np
 
 Select "Angular" as framework, will try "React" later
 
-- 
+- run `ionic generate page home`,
 
+Problem: error "Could not find module "@ionic/angular-toolkit""
 
+Solution: 
+
+```
+npm install -g @angular/cli
+npm install
+ionic generate page home
+ionic generate page index
+```
+- Generate Unauthenticated Pages
+
+```
+ionic generate page pages/welcome
+ionic generate page pages/login
+ionic generate page pages/signup
+
+```
+
+- Generate Authenticated Pages
+```
+ionic generate page pages/feed
+ionic generate page pages/messages
+ionic generate page pages/notifications
+ionic generate page pages/settings
+
+```
--- apple/src/app/app-routing.module.ts
+++ apple/src/app/app-routing.module.ts
@@ -5,7 +5,16 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'index', loadChildren: './index/index.module#IndexPageModule' },
+  { path: 'welcome', loadChildren: './pages/welcome/welcome.module#WelcomePageModule' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'signup', loadChildren: './pages/signup/signup.module#SignupPageModule' },
+  { path: 'feed', loadChildren: './pages/feed/feed.module#FeedPageModule' },
+  { path: 'notifications', loadChildren: './pages/notifications/notifications.module#NotificationsPageModule' },
+  { path: 'messages', loadChildren: './pages/messages/messages.module#MessagesPageModule' },
+  { path: 'settings', loadChildren: './pages/settings/settings.module#SettingsPageModule' }
 ];
 @NgModule({
   imports: [
